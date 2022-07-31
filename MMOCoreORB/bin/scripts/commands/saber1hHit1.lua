8719116362feaf70558b0db74750f383b9323195
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 12:54:28 -0500

    310722 jedi pass v1

--- MMOCoreORB/bin/scripts/commands/saber1hHit1.lua
+++ MMOCoreORB/bin/scripts/commands/saber1hHit1.lua
@@ -44,12 +44,12 @@
 Saber1hHit1Command = {
         name = "saber1hhit1",
 
-	damageMultiplier = 1.25,
+	damageMultiplier = 1.36,
 	speedMultiplier = 1.25,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
-	forceCostMultiplier = 1.0,
+	forceCostMultiplier = .97,
 	visMod = 25,
 	
 	animation = "combo_jedi_1", 
