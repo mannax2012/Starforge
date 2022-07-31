37474d566fff3c9d9e79c4b2d61122ca96acb424
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 15:54:46 -0500

    Update forceResistBleeding.lua

--- MMOCoreORB/bin/scripts/commands/forceResistBleeding.lua
+++ MMOCoreORB/bin/scripts/commands/forceResistBleeding.lua
@@ -44,7 +44,7 @@
 ForceResistBleedingCommand = {
 	name = "forceresistbleeding",
 	forceCost = 250,
-	duration = 900,
+	duration = 3600,
 	--animationCRC = hashCode()
 	clientEffect = "clienteffect/pl_force_resist_bleeding_self.cef",
 	frsLightBuffModifier = 0.35,
