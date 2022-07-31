9b9dd5d9a87b1c9e3833e678d481821768e5aaa9
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 15:54:11 -0500

    Update forceResistPoison.lua

--- MMOCoreORB/bin/scripts/commands/forceResistPoison.lua
+++ MMOCoreORB/bin/scripts/commands/forceResistPoison.lua
@@ -44,7 +44,7 @@
 ForceResistPoisonCommand = {
 	name = "forceresistpoison",
 	forceCost = 250,
-	duration = 900,
+	duration = 3600,
 	--animationCRC = hashCode()
 	clientEffect = "clienteffect/pl_force_resist_poison_self.cef",
 	frsLightBuffModifier = 0.35,
