2d651bbdb4bdc0f41e4af3ab430e92d0b236a6c7
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 15:47:27 -0500

    Update healHealthSelf2.lua

--- MMOCoreORB/bin/scripts/commands/healHealthSelf2.lua
+++ MMOCoreORB/bin/scripts/commands/healHealthSelf2.lua
@@ -44,9 +44,9 @@
 HealHealthSelf2Command = {
 	name = "healhealthself2",
 	healAmount = 1500,
-	forceCost = 25,
-	forceCostMultiplier = 0.05,
-	defaultTime = 6,
+	forceCost = 40,
+	--forceCostMultiplier = 0.05,
+	defaultTime = 1,
 	attributesToHeal = HEALTH_ATTRIBUTE,
 	range = 0,
 }
