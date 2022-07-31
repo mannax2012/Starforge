b87855efbb00bd6e0c84acbf4b7cdc44bcc25cad
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 15:54:31 -0500

    Update forceResistDisease.lua

--- MMOCoreORB/bin/scripts/commands/forceResistDisease.lua
+++ MMOCoreORB/bin/scripts/commands/forceResistDisease.lua
@@ -44,7 +44,7 @@
 ForceResistDiseaseCommand = {
 	name = "forceresistdisease",
 	forceCost = 250,
-	duration = 900,
+	duration = 3600,
 	--animationCRC = hashCode()
 	clientEffect = "clienteffect/pl_force_resist_disease_self.cef",
 	frsLightBuffModifier = 0.35,
