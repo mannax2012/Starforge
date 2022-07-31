8c4aa84aad4b88cf5255f5266ed778afde45151e
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 15:41:47 -0500

    Update healAllSelf1.lua

--- MMOCoreORB/bin/scripts/commands/healAllSelf1.lua
+++ MMOCoreORB/bin/scripts/commands/healAllSelf1.lua
@@ -46,7 +46,7 @@ HealAllSelf1Command = {
 	healAmount = 500,
 	forceCost = 125,
 	--forceCostMultiplier = 0.05, --makes healing flat rate
-	--defaultTime = 6, --if the player wants to nuke their force pool overhealing, let them
+	defaultTime = 1, --if the player wants to nuke their force pool overhealing, let them
 	attributesToHeal = HEALTH_ATTRIBUTE + MIND_ATTRIBUTE + ACTION_ATTRIBUTE,
 }
 
