560ceedb1fc85c1c22932d6beb3d55c57ff4444a
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 13:23:56 -0500

    310722 Jedi balance pass v1

--- MMOCoreORB/bin/scripts/commands/saber2hHit1.lua
+++ MMOCoreORB/bin/scripts/commands/saber2hHit1.lua
@@ -44,12 +44,12 @@
 Saber2hHit1Command = {
         name = "saber2hhit1",
 
-	damageMultiplier = 1.5,
+	damageMultiplier = 1.65,
 	speedMultiplier = 1.25,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
-	forceCostMultiplier = 1.0,
+	forceCostMultiplier = 1.05,
 	visMod = 25,
 
 	animation = "combo_2c", 
