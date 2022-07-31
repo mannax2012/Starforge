56a0fdd5fa84e0b030e752ef02427db2db9c361e
KimoSWG <109038466+KimoSWG@users.noreply.github.com>
2022-07-31 13:33:12 -0500

    Update saberPolearmLegHit1.lua

--- MMOCoreORB/bin/scripts/commands/saberPolearmLegHit1.lua
+++ MMOCoreORB/bin/scripts/commands/saberPolearmLegHit1.lua
@@ -44,12 +44,12 @@
 SaberPolearmLegHit1Command = {
         name = "saberpolearmleghit1",
       
-	damageMultiplier = 1.5,
+	damageMultiplier = 1.58,
 	speedMultiplier = 1.5,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
-	forceCostMultiplier = 1.25,
+	forceCostMultiplier = 1.31,
 	visMod = 25,
 
 	animation = "combo_3a", 
