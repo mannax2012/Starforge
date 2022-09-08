fa08668c76a51818af457a4a67fe4eab93a81a7d
mannax2012 <tincan2012@yahoo.com>
2022-09-08 19:59:07 +0100

    Force Meditate now Animates as you sit and Heals wounds

--- MMOCoreORB/src/server/zone/objects/player/events/ForceMeditateTask.h
+++ MMOCoreORB/src/server/zone/objects/player/events/ForceMeditateTask.h
@@ -8,6 +8,8 @@
 
 #include "engine/engine.h"
 #include "server/zone/objects/creature/CreatureObject.h"
+#include "server/chat/StringIdChatParameter.h"
+#include "templates/params/creature/CreatureAttribute.h"
 
 class ForceMeditateTask: public Task {
 	ManagedReference<CreatureObject*> player;
@@ -26,18 +28,55 @@ public:
 		return moodString;
 	}
 
-	void run() {
+		void run() {
 		Locker playerLocker(player);
 
 		try {
 			Reference<ForceMeditateTask*> fmeditateTask = player->getPendingTask("forcemeditate").castTo<ForceMeditateTask*>();
 
-			if (!player->isMeditating())
+			if (!player->isMeditating()){
 				return;
+			}
 
+			StringIdChatParameter healParams;
+			if (player->hasSkill("force_discipline_enhancements_synergy_04")){
+				//Here we are checking to see which pools have wounds, and we add them to a vector...
+				Vector<uint8> woundedPools;
+				for (uint8 i = 0; i < 9; ++i) {
+					if (player->getWounds(i) > 0){
+						woundedPools.add(i);
+					}
+				}
 
+				//Return without rescheduling because everything that can be healed has been?
+				if (woundedPools.size() > 0){
+
+					int heal = 40 + System::random(20);
+
+					// Select a random Attribute that has wounds...
+					uint8 pool = woundedPools.get(System::random(woundedPools.size() - 1));
+
+					int wounds = player->getWounds(pool);
+
+					//Cap the heal at the amount of wounds the creature has.
+					heal = Math::min(wounds, heal);
+
+					player->healWound(player, pool, heal, true, false);
+
+					// Sending System healing Message (wounds)
+					healParams.setStringId("teraskasi", "prose_curewound"); // [meditation] Your %TO wounds heal by %DI points.
+					healParams.setTO(CreatureAttribute::getName(pool));
+					healParams.setDI(heal);
+					player->sendSystemMessage(healParams);
+				}
+				
+			}
+
+			player->playEffect("clienteffect/pl_force_meditate_self.cef", "");
 			if (fmeditateTask != nullptr)
 				fmeditateTask->reschedule(5000);
+			else
+				fmeditateTask->schedule(5000);
 
 		} catch (Exception& e) {
 			player->error("unreported exception caught in ForceMeditateTask::activate");
